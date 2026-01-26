f27fbd703f415631c949607f6cb1c5dcf07372ee
Jonathan Mendoza <jonathan.v.mendoza@gmail.com>
2026-01-27 07:54:31 +0800

    Client Sessions Pages

---
+++ src/app/client_sessions/(articles)/financial_planning_session_with_a_breadwinner_mom_of_a_3_year_old_child/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import HeadlineComponent from "@/components/HeadlineComponent";
+import SpanEmoji from "@/components/SpanEmoji";
+import FreeConsultationForm from "@/components/FreeConsultationForm";
+import { useClientSessionsItem } from "@/contexts";
+
+function FinancialPlanningSessionWithABreadwinnerMomOfA3YearOldChild() {
+    const articleItem = useClientSessionsItem();
+    return (
+        <div className="flex flex-col items-stretch gap-4">
+            <p>Today, I had the privilege of talking to a strong breadwinner mom. <SpanEmoji value="💪❤️"/> She’s raising a 3-year-old child on her own, and she opened up about one of her biggest fears:</p>
+            <p className="italic">“What if something happens to me before my child becomes financially independent and builds a family of his own?”</p>
+            <p>To make things even harder, she doesn’t have a partner to share the responsibility. Every peso depends on her ability to work.</p>
+            <img src={articleItem.image}></img>
+
+            <hr/>
+            <HeadlineComponent emojiImg="emoji_u1f6e1.png" text="Building Her Safety Net" />
+            <div>
+                <p>After hearing her concerns, we designed a plan that complements her existing personal and company life insurance.</p>
+                <ul>
+                    <li><SpanEmoji value="&#x2705;&#xFE0F;"/> It provides additional protection.</li>
+                    <li><SpanEmoji value="&#x2705;&#xFE0F;"/> It includes income replacement in case the unexpected happens.</li>
+                    <li><SpanEmoji value="&#x2705;&#xFE0F;"/> And the best part—it comes with an investment-linked fund that grows over time. Something her child can use when he becomes an adult.</li>
+                </ul>
+            </div>
+
+            <hr/>
+            <HeadlineComponent emojiImg="emoji_u1f31f.png" text="Peace of Mind for Her and Her Child" />
+            <p>Now, she feels more at peace knowing she’s giving her child a stronger fighting chance in life—no matter what tomorrow brings.</p>
+
+            <hr/>
+            <HeadlineComponent emojiImg="emoji_u1f4e9.png" text="Your Turn" />
+            <p><SpanEmoji value="&#x1F4DD;"/> If you can relate to her story, <b>sign up for a free consultation</b> today. Let’s build a plan that gives you and your family peace of mind too.</p>
+            <FreeConsultationForm/>
+            <hr/>
+
+        </div>
+    );
+}
+
+export default FinancialPlanningSessionWithABreadwinnerMomOfA3YearOldChild;
---
+++ src/app/client_sessions/(articles)/layout.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import ContentComponent from "@/components/ContentComponent";
+import PagerButton from "@/components/PagerButton";
+import ScrollToTop from "@/components/ScrollToTop";
+import ArticleTitleComponent from "@/components/ArticleTitleComponent";
+import SpanEmoji from "@/components/SpanEmoji";
+import { useReadingProgress } from "@/utils/event_tracking";
+import { CLIENT_SESSIONS_ITEMS } from "@/constants";
+import { useEffect, useState } from "react";
+import { ClientSessionsItemContext } from "@/contexts";
+
+export default function Layout({children}: {children: React.ReactNode}) {
+    useReadingProgress();
+    const ITEMS = CLIENT_SESSIONS_ITEMS;
+    const pathname = usePathname();
+    const [articleIndex, setArticleIndex] = useState(ITEMS.findIndex(x => x.to === pathname));
+
+    useEffect(() => {
+        setArticleIndex(ITEMS.findIndex(x => x.to === pathname));
+    }, [pathname]);
+
+    if (articleIndex < 0) {
+        return (<></>);
+    }
+    const articleItem = ITEMS[articleIndex];
+
+    return (
+        <div>
+            <ScrollToTop/>
+            <ClientSessionsItemContext.Provider value={articleItem}>
+                <ContentComponent headline={<ArticleTitleComponent emojiImg={articleItem.emojiImg} text={articleItem.title}/>}>
+                    {children}
+                </ContentComponent>
+            </ClientSessionsItemContext.Provider>
+            <div className="w-full flex flex-row justify-between pt-8">
+                {articleIndex <= 0 && <PagerButton textLabel="Previous" textSubtext={<><SpanEmoji value="🤝"/> Client Sessions</>} to="/client_sessions" />}
+                {articleIndex > 0 && <PagerButton textLabel="Previous" textSubtext={<>{ITEMS[articleIndex - 1].spanEmoji}{` ${ITEMS[articleIndex - 1].title}`}</>} to={ITEMS[articleIndex - 1].to} />}
+                {articleIndex < (ITEMS.length - 1) && <PagerButton textLabel="Next" textSubtext={<>{ITEMS[articleIndex + 1].spanEmoji}{` ${ITEMS[articleIndex + 1].title}`}</>} to={ITEMS[articleIndex + 1].to} />}
+                {articleIndex == (ITEMS.length - 1) && <PagerButton textLabel="Next" textSubtext={<><SpanEmoji value="👤"/> About Me</>} to="/about_me" />}
+            </div>
+        </div>
+    );
+}
